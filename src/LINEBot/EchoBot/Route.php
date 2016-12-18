7d0b5b97bab6241fb76877d3a9f3ecce6d809413
Shunsky <shunsuke.iwmt@gmail.com>
2016-12-18 16:03:41 +0900

    no comment

--- src/LINEBot/EchoBot/Route.php
+++ src/LINEBot/EchoBot/Route.php
@@ -67,6 +67,10 @@ class Route
                 }
 
                 // $replyText = $event->getText();
+                $message = $event -> getTetx();
+
+                $res_contents = get_request($message);
+                // リクエスト
                 $replyText = 'うるせーばかやろー。';
                 $logger->info('Reply text: ' . $replyText);
                 $resp = $bot->replyText($event->getReplyToken(), $replyText);
@@ -76,5 +80,24 @@ class Route
             $res->write('OK');
             return $res;
         });
+    }
+
+    function get_request($param) {
+      $base_url = 'https://qiita.com';
+
+      $tag = 'PHP';
+      $query = ['page'=>'1','per_page'=>'5'];
+
+      $response = file_get_contents(
+                  $base_url.'/api/v2/tags/'.$tag.'/items?' .
+                  http_build_query($query)
+            );
+            // https://qiita.com/api/v2/tags/PHP/items?page=1&per_page=5
+
+      // 結果はjson形式で返されるので
+      $result = json_decode($response,true);
+
+      return $result;
+
     }
 }
